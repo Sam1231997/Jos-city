d64c3aeb29f7473e9de514c42e4048fac662d639
Sam1231997 <opyotusamuel2020@gmail.com>
2024-07-28 04:26:34 +0100

    another commit

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -31,7 +31,7 @@ const Dropdown = () => {
        <img src="/images/arrow.png" className="h-2 w-3 mt-2" />
       </button>
       {isOpen && (
-        <div className="absolute right-0 mt-2 w-48 bg-white border border-gray-200 rounded shadow-lg">
+        <div className="absolute right-0 mt-2 w-30 bg-white border border-gray-200 rounded shadow-lg">
           <Link to="/nowij" className="block px-4 py-2 text-green-800 hover:bg-gray-100">Current</Link>
           <Link to="/upcoming" className="block px-4 py-2 text-green-800 hover:bg-gray-100">Upcoming</Link>
          
